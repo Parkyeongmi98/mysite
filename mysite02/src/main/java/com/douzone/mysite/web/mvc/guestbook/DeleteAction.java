bd56534b3da1cc01f133ee84c4a79122fde663b2
Parkyeongmi98 <yeongmi_@naver.com>
2023-01-18 19:07:51 +0900

    guestbook #finish

---
+++ mysite02/src/main/java/com/douzone/mysite/web/mvc/guestbook/DeleteAction.java
@@ -0,0 +1,27 @@
+package com.douzone.mysite.web.mvc.guestbook;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.douzone.mysite.dao.GuestbookDao;
+import com.douzone.web.mvc.Action;
+
+public class DeleteAction implements Action {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String no1 = request.getParameter("no");
+		String password = request.getParameter("password");
+		
+		Long no = Long.parseLong(no1);
+		
+		new GuestbookDao().deleteByPassword(no, password);
+		
+		response.sendRedirect(request.getContextPath() + "/guestbook");
+
+	}
+
+}
