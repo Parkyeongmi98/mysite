593072b3a0cc01eb0fa4805cf13e5ebd7f35c23c
Parkyeongmi98 <yeongmi_@naver.com>
2023-02-01 14:58:27 +0900

    mybatis #4

--- mysite03/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
@@ -1,13 +1,7 @@
 package com.douzone.mysite.repository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.sql.DataSource;
+import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,134 +10,21 @@ import org.springframework.stereotype.Repository;
 import com.douzone.mysite.vo.GuestbookVo;
 
 @Repository
-public class GuestbookRepository {
-	@Autowired
-	private DataSource dataSource; // applicationContext에 설정한 Connection Pool DataSource
-	
+public class GuestbookRepository {	
 	@Autowired
 	private SqlSession sqlSession;
 	
 	public List<GuestbookVo> findAll() {
-		List<GuestbookVo> result = new ArrayList<>();
-		
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		
-		try {
-			conn = dataSource.getConnection();
-			
-			// 3. Statement 준비
-			String sql = "select no, name, password, message, date_format(reg_date, '%Y-%m-%d %H:%i:%s')" 
-			+ " from guestbook" 
-			+ " order by reg_date desc";
-			pstmt = conn.prepareStatement(sql);
-			
-			
-			// 4. SQL 실행
-			rs = pstmt.executeQuery(); 
-			
-			// 5. 결과 처리
-			while (rs.next()) {
-				GuestbookVo vo = new GuestbookVo();
-				vo.setNo(rs.getLong(1));
-				vo.setName(rs.getString(2));
-				vo.setPassword(rs.getString(3));
-				vo.setMessage(rs.getString(4));
-				vo.setRegDate(rs.getString(5));
-
-				result.add(vo);
-			}
-			
-		} catch (SQLException e) {
-			System.out.println("error: " + e);;
-		} finally {
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		return result;
+		return sqlSession.selectList("guestbook.findAll");
 	}
 
 	public void insert(GuestbookVo vo) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		
-		try {
-			conn = dataSource.getConnection();
-			
-			// 3. Statement 준비
-			String sql = "insert into guestbook values(null, ?, ?, ?, sysdate())";
-			pstmt = conn.prepareStatement(sql);
-			
-			pstmt.setString(1, vo.getName());
-			pstmt.setString(2, vo.getPassword());
-			pstmt.setString(3, vo.getMessage());
-
-	
-			// 4. SQL 실행
-			pstmt.executeUpdate();
-			
-		} catch (SQLException e) {
-			System.out.println("error: " + e);;
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
+		sqlSession.insert("guestbook.insert", vo);
 	}
 	
 	public void deleteByPassword(Long no, String password) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		
-		try {
-			conn = dataSource.getConnection();
-			
-			// 3. Statement 준비
-			String sql = "delete from guestbook where no = ? and password = ?";
-			pstmt = conn.prepareStatement(sql);
-			
-			pstmt.setLong(1, no);
-			pstmt.setString(2, password);
-			
-			// 5. SQL 실행
-			pstmt.executeUpdate(); 
-			
-		} catch (SQLException e) {
-			System.out.println("error: " + e);;
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		
+		Map<String, Object> map = Map.of("no", no, "password", password);
+		sqlSession.delete("guestbook.deleteByPassword", map);
 	}
 
 }
